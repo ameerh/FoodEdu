f53aba6aba33b16f426fb4345eda4e16d4b346fa
Yasir Mughal <yasir.rafiq@novatoresols.com>
2016-06-17 12:35:41 +0500

    port change 4005

--- config.js
+++ config.js
@@ -11,8 +11,8 @@
 
 module.exports = {
     "siteName": "footeducation",
-    "siteRoot": "http://localhost:8080",
-    "sitePort": 8080,
+    "siteRoot": "http://localhost:4005",
+    "sitePort": 4005,
     "logging": {
         "level": "info"
     },
@@ -61,7 +61,7 @@ module.exports = {
     },
     "multisite": {
         "enabled": false,
-        "globalRoot": "http://global.localhost:8080"
+        "globalRoot": "http://global.localhost:4005"
     },
     "siteIP": "127.0.0.1"
 };

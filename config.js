9ba4e9f791f519b5f6f092d6daa8571434d15d79
Yasir Mughal <yasir.rafiq@novatoresols.com>
2016-06-22 16:23:34 +0500

    custom page done

--- config.js
+++ config.js
@@ -11,7 +11,7 @@
 
 module.exports = {
     "siteName": "footeducation",
-    "siteRoot": "http://52.38.184.71:8080",
+    "siteRoot": "https://52.38.184.71:8080",
     //"siteRoot": "http://localhost:8080",
     "sitePort": 8080,
     "logging": {

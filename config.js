aa6667a56e71a6dccaf49b94e3fd026f91ec4fd0
Yasir Mughal <yasir.rafiq@novatoresols.com>
2016-07-12 18:06:57 +0500

    hover side-nav etcdone

--- config.js
+++ config.js
@@ -11,9 +11,9 @@
 
 module.exports = {
     "siteName": "footeducation",
-    "siteRoot": "https://52.38.184.71",
-    //"siteRoot": "http://localhost:8080",
-    "sitePort": 80,
+    //"siteRoot": "https://52.38.184.71",
+    "siteRoot": "http://localhost:8080",
+    "sitePort": 8080,
     "logging": {
         "level": "info"
     },

b2eab6141390308b4b479f0b13a9a31e9d055936
Yasir Mughal <yasir.rafiq@novatoresols.com>
2017-03-02 12:15:55 +0500

    beta.footeducation.com deployment

--- config.js
+++ config.js
@@ -11,10 +11,10 @@
 
 module.exports = {
     "siteName": "Foot Education",
-    // "siteRoot": "http://localhost:8080",
-    // "sitePort": 8080,
+     //"siteRoot": "http://localhost:8080",
+     //"sitePort": 8080,
     //"siteRoot": "https://footeducation.us-east-1.elasticbeanstalk.com",
-    "siteRoot": "www.beta.footeducation.com",
+    "siteRoot": "beta.footeducation.com",
     "sitePort": process.env.PORT,
     "logging": {
         "level": "info"

af64b5b49a2d76181db29b023c8061349de512cb
Yasir Mughal <yasir.rafiq@novatoresols.com>
2017-03-03 19:08:23 +0500

    Webmaster verification and Anatomy page

--- plugins/contact-pencilblue/public/js/pb_contact_form.js
+++ plugins/contact-pencilblue/public/js/pb_contact_form.js
@@ -1,7 +1,39 @@
+function captchaCode() {
+  var Numb1, Numb2, Numb3, Numb4, Numb6, Numb5, Code;
+  Numb1 = (Math.ceil(Math.random() * 10)-1).toString();
+  Numb2 = (Math.ceil(Math.random() * 10)-1).toString();
+  Numb3 = (Math.ceil(Math.random() * 10)-1).toString();
+  Numb4 = (Math.ceil(Math.random() * 10)-1).toString();
+  Numb5 = (Math.ceil(Math.random() * 10)-1).toString();
+  Numb6 = (Math.ceil(Math.random() * 10)-1).toString();
+
+  Code = Numb1 + Numb2 + Numb3 + Numb4 + Numb5 + Numb6;
+  $("#captcha span").remove();
+  $("#captcha input").remove();
+  $("#captcha").append("<span id='code'>" + Code + "</span><input type='button' onclick='captchaCode();'>");
+}
+
 (function() {
+
+  captchaCode();
+  $("#pb_contact_submit").style.display = "none";
+
   $('#pb_contact_form').validate();
 
   submitPBContact = function() {
+    var captchaVal = $("#code").text();
+    var captchaCode = $(".captcha").val();
+    if (captchaVal == captchaCode) {
+      $("#pb_contact_submit").style.display = "block";
+      $(".captcha").css({
+        "color" : "#7CEA19"
+      });
+    }
+    else {
+      $(".captcha").css({
+        "color" : "#CE3B46"
+      });
+    }
     if(!$('#pb_contact_name').val().length || !$('#pb_contact_email').val().length) {
       $('#pb_contact_invalid').show();
       return;

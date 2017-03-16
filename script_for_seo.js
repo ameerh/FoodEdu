655cde2f97833891b092f957182877b81f6282c0
Wasif Ali <wasif.ali@novatoresols.com>
2017-03-16 15:38:48 +0500

    redirection of social icons to new tab

--- script_for_seo.js
+++ script_for_seo.js
@@ -4,6 +4,7 @@ var async = require('async')
 
 // Connection URL
 var url = 'mongodb://footeducation:Jan12017@ds133549-a0.mlab.com:33549/footeducation';
+// var url = 'mongodb://127.0.0.1/footeducationpages';
 
 MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
@@ -15,26 +16,27 @@ MongoClient.connect(url, function(err, db) {
             else {
                 async.eachLimit(pages, pages.length , function(page, callback){
 
-                    var page_lay = page.page_layout.replace(/http:\/\/www.footeducation.com/g, "https://www.footeducation.com");
+                    var page_lay = page.page_layout;
+                    var page_lay = page.page_layout.replace(/href="\//g, "href=\"https://www.footeducation.com/page/");
                     // console.log(page_lay+ '\n\n\n\n')
 
-                    db.collection('page').update(
-                        {
-                            _id : page._id
-                        },
-                        {
-                            $set: {page_layout : page_lay}
-
-                        },
-                        function(err){
-                            if(err){
-                                callback(err)
-                            }
-                            else{
-                                callback()
-                            }
-                        }
-                    );
+                    // db.collection('page').update(
+                    //     {
+                    //         _id : page._id
+                    //     },
+                    //     {
+                    //         $set: {page_layout : page_lay}
+                    //
+                    //     },
+                    //     function(err){
+                    //         if(err){
+                    //             callback(err)
+                    //         }
+                    //         else{
+                    //             callback()
+                    //         }
+                    //     }
+                    // );
                 }, function(err){
                     if(err){
                         console.log(err)

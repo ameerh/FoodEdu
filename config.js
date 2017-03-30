c3aa74a80830c5ac1ecee0e27d5ec55c12e941cf
ameerh <ameer@novatoresols.com>
2017-03-30 15:41:54 +0500

    Update config.js

--- config.js
+++ config.js
@@ -13,9 +13,9 @@ module.exports = {
     "siteName": "footEducation",
      // "siteRoot": "http://localhost:8080",
      // "sitePort": 8080,
-    //"siteRoot": "http://footeducation-staging.us-east-1.elasticbeanstalk.com/",
+    "siteRoot": "http://footeducation-staging.us-east-1.elasticbeanstalk.com/",
     "sitePort": process.env.PORT,
-    "siteRoot": "https://www.footeducation.com/",
+    //"siteRoot": "https://www.footeducation.com/",
     "logging": {
         "level": "info"
     },

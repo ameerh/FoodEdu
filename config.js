35105cfd7ac2ee7e2ccb567b2e3b7e568191863f
Wasif Ali <wasif.ali@novatoresols.com>
2017-04-19 12:36:31 +0500

    Links styling issue resolved

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ module.exports = {
     // "siteRoot": "http://localhost:8080",
     "siteRoot": "http://footeducation-staging.us-east-1.elasticbeanstalk.com/",
     "sitePort": process.env.PORT || '8080',
-//     "siteRoot": "https://www.footeducation.com/",
+    // "siteRoot": "https://www.footeducation.com/",
     "logging": {
         "level": "info"
     },
